c6e81fa4f62e9931b6ad18073e2b8d32145f9a33
langermank <langermank@gmail.com>
2019-10-21 10:37:19 -0400

    more shtuff

--- langermank/src/pages/page-2.js
+++ langermank/src/pages/page-2.js
@@ -10,9 +10,30 @@ const SecondPage = () => (
 			<p>Turn on your dev tools inspector in firefox to see the magic</p>
 		</section>
 		<section>
+			<h3>Katie as: the space-between</h3>
 			<div className={styles.sec1}>
 				<div><h2>Designer</h2></div>
 				<div><h2>Developer</h2></div>
+			</div>
+		</section>
+		<section>
+			<h3>Zoraida as: the space-between</h3>
+			<div className={styles.sec2}>
+				<span className={styles.skillz}>
+					<div><h2>Designer</h2></div>
+					<div><h2>Developer</h2></div>
+				</span>
+				<div><h2>Researcher</h2></div>
+			</div>
+		</section>
+		<section>
+			<h3>I need some room to breathe!</h3>
+			<div className={styles.sec3}>
+				<div className={styles.solidBox}></div>
+				<div className={styles.solidBox}></div>
+				<div className={styles.solidBox}></div>
+				<div className={styles.solidBox}></div>
+				<div className={styles.solidBox}></div>
 			</div>
 		</section>
 		<section>
