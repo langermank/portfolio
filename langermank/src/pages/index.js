e2f85a95355417b0d5cf1f32f460029b3f14a713
langermank <langermank@gmail.com>
2019-11-03 15:16:28 -0500

    some content

--- langermank/src/pages/index.js
+++ langermank/src/pages/index.js
@@ -1,20 +1,20 @@
 import React, { Fragment } from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
 import styles from "../styles/home.module.scss"
 
 const IndexPage = props => (
-  <Fragment>
+  <div className={styles.pageWrap}>
   <header>
     <h1>Katie Langerman</h1>
     <span className={styles.navLinksWrap}>
       <a href="mailto:langermank@gmail.com" alt="email katie">langermank@gmail.com</a>
       <span className={styles.navLinks}>
-        <a href="https://www.linkedin.com/in/langermank/" alt="linkedin">linkedin</a>
-        <a href="https://twitter.com/KatieLangerman" alt="twitter">twitter</a>
-        <a href="https://github.com/langermank" alt="github">github</a>
-        <a href="https://dribbble.com/katielangerman" alt="dribbble">dribbble</a>
+        <a href="https://www.linkedin.com/in/langermank/" alt="linkedin" target="_blank">linkedin</a>
+        <a href="https://twitter.com/KatieLangerman" alt="twitter" target="_blank">twitter</a>
+        <a href="https://github.com/langermank" alt="github" target="_blank">github</a>
+        <a href="https://dribbble.com/katielangerman" target="_blank" alt="dribbble">dribbble</a>
       </span>
     </span>
   </header>
@@ -23,14 +23,15 @@ const IndexPage = props => (
     <div className={styles.aboutKatie}>
       <Img fixed={props.data.imageOne.childImageSharp.fixed} />
       <p>
-        Katie is a Senior UI/UX Engineer at <a href="https://careers.cargurus.com/">CarGurus</a> and a co-organizer
-        of <a href="https://www.meetup.com/Ladies-That-UX-Boston/">Ladies that UX Boston</a>, a meetup group empowering women who work in design and technology. With a background in Graphic Design, she both designs and develops products that are performant and user friendly. She’s passionate about connecting engineering to design through the magic of CSS. Currently, she's nerding out over design systems and reusable components.
+        Katie is a Senior UI/UX Engineer at <a href="https://careers.cargurus.com/" alt="CarGurus" target="_blank">CarGurus</a> and a co-organizer
+        of <a href="https://www.meetup.com/Ladies-That-UX-Boston/" alt="Ladies that UX Boston Meetup Group" target="_blank">Ladies that UX Boston</a>, a meetup group empowering women who work in design and technology. With a background in Graphic Design, she both designs and develops products that are performant and user friendly. She’s passionate about connecting engineering to design through the magic of CSS. Currently, she's nerding out over design systems and reusable components.
       </p>
     </div>
-    <div className={styles.moreThings}>meat and potatoes</div>
-    <footer>Hello</footer>
+    <div className={styles.moreThings}>
+    </div>
   </main>
-  </Fragment>
+  <footer><Link to="page-2" alt="my scrap paper">Playground</Link></footer>
+  </div>
 )
 
 export const pageQuery = graphql`
